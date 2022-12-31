96410b009c7a03d480d4b7c336c9afc455ee7c10
meyson <meyson.rylit@gmail.com>
2023-01-01 01:15:02 +0200

    merge

--- Controller.php
+++ Controller.php
@@ -4,15 +4,25 @@ namespace app\core;
 
 class Controller
 {
+    /**
+     * Renders requested view
+     * @param $view
+     * @param $params
+     * @return string|string[]
+     */
     protected function render($view, $params)
     {
        return Application::$app->router->renderView($view, $params);
     }
 
-    protected function redirect($rout)
+    /**
+     * Redirects to requested rout
+     * @param $route
+     */
+    protected function redirect($route)
     {
         $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-        header("Location: $link/$rout");
+        header("Location: $link/$route");
         exit();
     }
 }
