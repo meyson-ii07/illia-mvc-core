2c485eff85c8864ae0fa3f18a85e2f3875759a03
meyson <meyson.rylit@gmail.com>
2023-02-23 16:35:36 +0200

    moved application initialisation to core package

--- src/Application.php
+++ src/Application.php
@@ -2,7 +2,9 @@
 
 namespace Meyson\IlliaMvcCore;
 
+use Dotenv\Dotenv;
 use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 class Application
 {
@@ -31,6 +33,55 @@ class Application
         $this->router = new Router($this->request, $this->response);
         $this->db = new Database($config['db']);
     }
+
+
+    public static function initialiseApp()
+    {
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__)."/../../../");
+        $dotenv->load();
+        $config = [
+            'db' => [
+                'dsn' => $_ENV['DB_DSN'],
+                'user' => $_ENV['DB_USER'],
+                'password' => $_ENV['DB_PASSWORD'],
+            ],
+        ];
+        $isDebug = $_ENV['APP_ENV'] == 'debug';
+
+        if ($isDebug) {
+            $twigConf = [
+                'debug' => true,
+                'cache' => false,
+                'auto_reload' => true,
+            ];
+        } else {
+            $twigConf = [
+                'debug' => false,
+                'cache' => '../var/cache',
+                'auto_reload' => true,
+            ];
+        }
+
+        try {
+            $loader = new FilesystemLoader('../views');
+            $twig = new Environment($loader, $twigConf);
+
+
+            $app = new Application(dirname(__DIR__)."/../../../", $config);
+            $app->session->setCsrfToken();
+            $app->router->loadRoutes();
+            $app->twig = $twig;
+            $app->run();
+        }
+        catch (Exception $e) {
+            if ($isDebug) {
+                dd($e->getMessage()."\n".$e->getTraceAsString());
+            } else {
+                $twig->render('404_.html.twig');
+            }
+        }
+    }
+
 
     /**
      *  Echoes content of the page
